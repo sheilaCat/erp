a16d5b487794439180b0d11b333eade4b3e9c5e6
sheilaCat <326278902@qq.com>
2016-05-14 16:02:02 +0800

    set logger

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var errorhandler = require('errorhandler');
 require('./model');
 var auth = require('./middleware/auth');
 var MongoClient = require('mongodb').MongoClient;
+var log4js = require('log4js');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 
@@ -20,10 +20,28 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs-mate'));
 
+//log4js
+log4js.configure({
+  appenders: [
+    { type: 'console' }, //控制台输出
+    {
+      type: 'file', //文件输出
+      filename: 'access.log',
+      maxLogSize: 0,
+      backups: 4,
+      catagory: 'normal'
+    }
+  ],
+  replaceConsole: true
+});
+
+var logger = log4js.getLogger('normal');
+logger.setLevel('INFO');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+/*app.use(logger('dev'));
+*/app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('ERP'));
@@ -38,8 +56,12 @@ app.use(session({
 }));
 app.use(auth.authUser);
 
+/*app.use(log4js.connectLogger(logger, {level:log4js.levels.INFO}));*/
+// app.use(log4js.connectLogger(logger, {level: log4js.levels.INFO, format:':method :url'}));
+app.use(log4js.connectLogger(logger, {level: log4js.levels.INFO, format:':method :url'}));
 //routers
 app.use('/', webRouter);
+
 
 // catch 404 and forward to error handler
 /*app.use(function(req, res, next) {
@@ -60,3 +82,9 @@ if (app.get('env') === 'development') {
 
 
 module.exports = app;
+
+/*exports.logger = function(name) {
+  var logger = log4js.getLogger(name);
+  logger.setLevel('INFO');
+  return logger;
+}*/
